82e9c8530c313a329568b1666121120c815c20ab
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-10 19:02:24 +0300

    newUp

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import css from "./ContactForm.module.css";
+
+export const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const target = event.currentTarget.name;
+    switch (target) {
+      case 'name':
+        setName(event.currentTarget.value);
+        break;
+
+      case 'number':
+        setNumber(event.currentTarget.value);
+        break;
+
+      default:
+        console.log('Something wrong');
+        break;
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    onSubmit({ name: name, number: number });
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+
+    return(
+     <form onSubmit={handleSubmit} className={css.form}>
+        <label className={css.subTitle}>
+          Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-ЯІіЇїҐґ' \-\u0400-\u04FF]+$"
+            title="Name may contain only letters, apostrophe, dash and spaces. 
+          For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            placeholder="Enter name"
+            value={name}
+            onChange={handleChange}
+          />
+        </label>
+
+        <label className={css.subTitle}>
+          Number
+          <input
+            className={css.input}
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            placeholder="Enter your number"
+            value={number}
+                onChange={handleChange}
+             required
+          />
+        </label>
+
+        <button type="submit" className={css.btn}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
+
+
+ContactForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+};
