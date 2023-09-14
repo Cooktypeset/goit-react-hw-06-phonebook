3753317e2b8eaef71281728100431f63a68f93fa
Cooktypeset <tovpyhalitva@gmail.com>
2023-09-14 10:55:43 +0300

    newUpdfix

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,2 +1,19 @@
-export const getContacts = state => state.contacts.contacts;
-export const getFilter = state => state.filter;
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { ContactsSliceReduser } from './ContactsSlice/ContactsSlice';
+import { FilterReduser } from './FilterSlice/FilterSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
